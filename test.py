272b21a471c5b9b7d114891b33dbae78804aad63
Jae-Mo Lihm <jaemolim96@gmail.com>
2019-08-22 13:09:58 +0900

    Initial commit for SupercellPy

---
+++ test.py
@@ -0,0 +1,34 @@
+import numpy as np
+from pwinput import PWInput
+from make_supercell import make_fd_supercell
+"""
+Test example for using make_fd_supercell
+"""
+
+BOHR_TO_ANG = 0.529177
+
+# read pw.x input to PWInput object
+pw = PWInput.from_file('test.in')
+
+# Define perturbation
+# phonon crystal momentum
+q = [0, 1/2, 1/3]
+# phonon displacement mode
+u = np.zeros((3, 2), dtype=complex)
+u[:, 0] = [0.499528 + 0.024785j, 0.499240 + 0.024834j, -0.000288 + 0.000049j]
+u[:, 1] = [0.488266 + 0.108347j, 0.487974 + 0.108347j, -0.000292 + 0.000000j]
+# normalize phonon displacement
+amplitude = 1E-3 # angstrom
+alat = pw.get_alat()  # bohr
+u *= amplitude / (alat * BOHR_TO_ANG) / np.linalg.norm(u)
+
+# make supercell
+pw_super = make_fd_supercell(pw, q, u)
+
+# write pw.x input object for supercell
+pw_super.write_file('test_super.in')
+
+print(pw_super.cell)
+print(pw_super.atoms_name)
+for i in range(pw_super.nat):
+    print(pw_super.atoms_cart[:, i])
